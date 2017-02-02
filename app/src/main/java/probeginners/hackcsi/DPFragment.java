3223b58dc500a7be19d72d8fdb864278b9742f9b
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-02 11:00:08 +0530

    initial fragment

--- app/src/main/java/probeginners/hackcsi/DPFragment.java
+++ app/src/main/java/probeginners/hackcsi/DPFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
  */
 public class DPFragment extends Fragment {
 
+    Button DONATE, PURCHASE;
 
     public DPFragment() {
         // Required empty public constructor
@@ -23,7 +25,26 @@ public class DPFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_d, container, false);
+        View v = inflater.inflate(R.layout.fragment_d, container, false);
+
+        DONATE = (Button) v.findViewById(R.id.DONATE);
+        PURCHASE = (Button) v.findViewById(R.id.PURCHASE);
+
+        DONATE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        PURCHASE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return v;
     }
 
 }
