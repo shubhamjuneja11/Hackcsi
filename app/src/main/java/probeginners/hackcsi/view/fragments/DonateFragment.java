ed21b50e4fd09a73d6a256847c2b730afcd2b576
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-02 12:32:44 +0530

    swipe tabs

--- app/src/main/java/probeginners/hackcsi/view/fragments/DonateFragment.java
+++ app/src/main/java/probeginners/hackcsi/view/fragments/DonateFragment.java
@@ -15,6 +15,9 @@ import probeginners.hackcsi.R;
  */
 public class DonateFragment extends Fragment {
 
+
+    EditText b_name, b_author, b_mrp, b_year, y_name, y_address, y_phone;
+
     public DonateFragment() {
         // Required empty public constructor
     }
@@ -24,7 +27,17 @@ public class DonateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donate, container, false);
+        View v = inflater.inflate(R.layout.fragment_donate, container, false);
+
+        b_name = (EditText) v.findViewById(R.id.b_name);
+        b_author = (EditText) v.findViewById(R.id.b_author);
+        b_mrp = (EditText) v.findViewById(R.id.b_mrp);
+        b_year = (EditText) v.findViewById(R.id.b_year);
+        y_name = (EditText) v.findViewById(R.id.y_name);
+        y_address = (EditText) v.findViewById(R.id.y_address);
+        y_phone = (EditText) v.findViewById(R.id.y_phone);
+
+        return v;
     }
 
 }
