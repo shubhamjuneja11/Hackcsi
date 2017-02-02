81784104443534d158855635c6e0ca8ea8020db3
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-02-02 15:25:28 +0530

    ...

---
+++ app/src/main/java/probeginners/hackcsi/view/activities/ChatActivity.java
@@ -0,0 +1,15 @@
+package probeginners.hackcsi.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import probeginners.hackcsi.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
